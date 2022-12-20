092c3bfdcb8171e1dfacadeb7b05b0723aa3a14c
Pavel Nedviga <Pavel.Nedviga@gmail.com>
2022-12-20 16:18:43 +0300

    TSEP Installator

---
+++ TeklaHierarchicDefinitions/install support.cs
@@ -0,0 +1,11 @@
+﻿#pragma warning disable 1633 // Unrecognized #pragma directive
+#pragma reference "Tekla.Macros.Runtime"
+#pragma warning restore 1633 // Unrecognized #pragma directive
+
+namespace UserMacros {
+    public sealed class Macro {
+        [Tekla.Macros.Runtime.MacroEntryPointAttribute()]
+        public static void Run(Tekla.Macros.Runtime.IMacroRuntime runtime) {
+        }
+    }
+}
